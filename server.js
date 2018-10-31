173cfb288f2ecb5a6d451431246454cb6ba2470e
jatin-sofocle <jatin.agrawal@sofocle.com>
2018-10-31 17:43:13 +0530

    Ball Speeding issue fixed

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const moment = require('moment');
+const http = require('http');
+const socketIO = require('socket.io');
 const app = express();
+
+
+var server = http.createServer(app);
+var io = socketIO(server);
 
 
 app.get('/', (req, res) => {
@@ -8,7 +14,17 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/test', (req, res) => {
+  console.log(moment().format('MMM DD, YYYY k:mm:ss ::'), req.method, ' ', req.url);
+  res.sendFile(__dirname + '/views/test.html');
+});
 
-app.listen(3000, () => {
+app.get('/test-test', (req, res) => {
+    console.log(moment().format('MMM DD, YYYY k:mm:ss ::'), req.method, ' ', req.url);
+    res.sendFile(__dirname + '/views/test-test.html');
+});
+
+
+server.listen(3000, () => {
   console.log('Server up at port 3000 ;)');
 });
